6511a1c99066d6acd2f34b5c35c6b04efc8f09d6
MonsterOrion <gurjot4981@gmail.com>
2026-01-08 20:31:35 +0530

    jojfoapjfas
    afasfafafs

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+ 
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public void Start()
+    {   
+        LoadVolume();
+        MusicManager.Instance.PlayMusic("MainMenuBGM");
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("MainGame");
+        MusicManager.Instance.PlayMusic("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+ 
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+ 
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+ 
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+    }
+ 
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+    }
+
+}
